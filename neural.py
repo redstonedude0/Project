eac1a395344e6d364869c53ab5c87a763a6c5f58
harrison <hrjh2@cam.ac.uk>
2020-11-27 15:03:25 +0000

    Added: - neural network code - LBP code
    TODO:
    - LBP code needs finishing
    - NN code needs pythonising

--- neural.py
+++ neural.py
@@ -39,7 +39,7 @@ class NeuralNet(nn.Module):
             # TODO - absolutely no idea how to do this, this is a filler for now
         )  # Attention mechanism to achieve feature representation
         self.f_m_c = nn.Sequential(
-            nn.Linear(300, 100),
+            nn.Linear(300, 100),  # TODO what dimensions?
             nn.Tanh(),
             nn.Dropout(p=SETTINGS.dropout_rate),
         )
@@ -112,11 +112,84 @@ class NeuralNet(nn.Module):
         x = y / np.math.sqrt(SETTINGS.d)
         return np.math.exp(x)
 
-    def forward(self, x, y):
-        for m in []:  # all mentions
-            for e in []:  # candidate entities
-                p_e_m = x  # input from data
-                q_e_d = y  # From LBP
+    '''
+    m and mbar from t-1
+    m & mbar: [i][j][arg]'''
+
+    # LBP FROM https://arxiv.org/pdf/1704.04920.pdf
+    def lbp_iteration_individual(self, mbar, i, j, arg, candidates, B, R, D):
+        # TODO ensure candidates is Gamma(i) - Gamma is the set of candidates for a mention?
+        maxValue = 0
+        max = None
+        for e_prime in candidates:
+            value = self.psi(i, e_prime, B)
+            m_i, m_j = None, None  # Find mentions from entity???? TODO - how to do
+            value += self.phi(arg, e_prime, m_i, m_j, R, D)
+            mentions = []  # TODO - set of mentions
+            for k in mentions:
+                if k != j:
+                    value += mbar[k][i][e_prime]
+            if value > maxValue:
+                max = e_prime
+        return max
+
+    def lbp_iteration_complete(self, mbar, candidates, B, R, D):
+        newmbar = {}
+        mentions = []  # TODO - all mentions
+        for i in mentions:
+            newmbar[i] = {}
+            for j in mentions:
+                newmbar[i][j] = {}
+                mvalues = {}
+                for arg in mentions:
+                    newmval = self.lbp_iteration_individual(mbar, i, j, arg, candidates, B, R, D)
+                    mvalues[arg] = newmval
+                mvalsum = 0
+                for value in mvalues.values():
+                    mvalsum += np.exp(value)
+                for arg in mentions:
+                    # Bar (needs softmax)
+                    dampingFactor = 0.5  # TODO - what value for delta?
+                    mval = mvalues[arg]
+                    bar = np.log(
+                        dampingFactor * (np.exp(mval) / mvalsum) + (1 - dampingFactor) * np.exp(mbar[i][j][arg]))
+                    newmbar[i][j][arg] = bar
+        return newmbar
+
+    def lbp_total(self, candidates, B, R, D):
+        mbar = {}
+        mentions = []  # TODO - all mentions
+        for i in mentions:
+            mbar[i] = {}
+            for j in mentions:
+                mbar[i][j] = 0
+        for loopno in range(0, SETTINGS.LBP_loops):
+            newmbar = self.lbp_iteration_complete(mbar, candidates, B, R, D)
+            mbar = newmbar
+        # Now compute ubar
+        ubar = {}
+        for i in mentions:
+            ubar[i] = {}
+            for arg in mentions:
+                sum = 0
+                for k in mentions:
+                    if k != i:
+                        sum += mbar[k][i][arg]
+                u = self.psi(i, arg, B) + sum
+                ubar[i][arg] = np.exp(u)
+            sum = 0
+            for arg in mentions:  # Gamma(mi)
+                sum += ubar[i][arg]
+            for arg in mentions:
+                ubar[i][arg] /= sum  # Normalise
+        return ubar
+
+    def forward(self, mentions, y, candidates, B, R, D):
+        ubar = self.lbp_total(candidates, B, R, D)
+        for m in mentions:  # all mentions
+            for e in m.candidates:  # candidate entities
+                p_e_m = e.initial_prob  # input from data
+                q_e_d = ubar[i][e]  # From LBP
                 g = lambda x, y: None  # 2-layer NN
                 p_e = g(q_e_d, p_e_m)
                 #return all p_e for all m
