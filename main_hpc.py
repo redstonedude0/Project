b71593f6522be31f5fb6c04dfde359e8c42c1b1b
harrison <hrjh2@cam.ac.uk>
2021-01-14 17:10:31 +0000

    Added: - main_hpc runs main

--- main_hpc.py
+++ main_hpc.py
@@ -5,4 +5,6 @@ from hyperparameters import SETTINGS
 SETTINGS.dataDir = "/rds/user/hrjh2/hpc-work/"
 SETTINGS.dataDir_csv = "/rds/user/hrjh2/hpc-work/generated/test_train_data/"
 SETTINGS.dataDir_embeddings = "/rds/user/hrjh2/hpc-work/generated/word_ent_embs/"
-# error?
+import main
+
+print(main.results)
