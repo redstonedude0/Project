4c9eccb31e6dcf1759bf37eed27a0f8779899f4d
harrison <hrjh2@cam.ac.uk>
2021-01-15 00:38:38 +0000

    Added: - main_hpc correct dir

--- main_hpc.py
+++ main_hpc.py
@@ -4,7 +4,7 @@ from hyperparameters import SETTINGS
 
 SETTINGS.dataDir = "/rds/user/hrjh2/hpc-work/"
 SETTINGS.dataDir_csv = "/rds/user/hrjh2/hpc-work/generated/test_train_data/"
-SETTINGS.dataDir_embeddings = "/rds/user/hrjh2/hpc-work/generated/word_ent_embs/"
+SETTINGS.dataDir_embeddings = "/rds/user/hrjh2/hpc-work/generated/embeddings/word_ent_embs/"
 import main
 
 print(main.results)
